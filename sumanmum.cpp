90b0598979afd5b6c7d524b82536900b097b118e
Franfmga2022 <113040688+Franfmga2022@users.noreply.github.com>
2022-10-29 10:08:35 -0500

    Update sumanmum.cpp

--- sumanmum.cpp
+++ sumanmum.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
   float y,s=0.0;
-  int i=0,l;
-  cout<<"Ingrese el valor de l: ";
-  cin>>l;
+  int i=0,c;
+  cout<<"Ingrese el valor de c: ";
+  cin>>c;
   do{
 
   cout<<"Ingrese el valor de y: ";
@@ -15,7 +15,7 @@ int main()
   s=s+y;
 
 
- }while(i<l);
+ }while(i<c);
 
  cout<<"La suma de los numeros fue: "<<s<<endl;
   return 0;
