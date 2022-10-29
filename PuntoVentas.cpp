c6aef57cdf8d264f6f784f8758b925dc235c61a8
Franfmga2022 <113040688+Franfmga2022@users.noreply.github.com>
2022-10-29 09:41:55 -0500

    Update PuntoVentas.cpp

--- PuntoVentas.cpp
+++ PuntoVentas.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-	float x,s=0,vb,piva=12,viva,pdesc=10,vdesc,vn;
+	float y,s=0,vb,piva=12,viva,pdesc=10,vdesc,vn;
 	int i=0,l;
 	cout<<"Ingrese l : ";
 	cin>>l;
 	do{
-		cout<<"Ingrese x : ";
-		cin>>x;
+		cout<<"Ingrese y : ";
+		cin>>y;
 		i=i+1;
-		s=s+x;
+		s=s+y;
 	  }while(i<l);
 	   vb=s;
 	   viva=vb*piva/100;
