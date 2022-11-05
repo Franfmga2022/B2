9e1b6880bc2f8e822f6602d8db0d25a6f07d7593
Franfmga2022 <113040688+Franfmga2022@users.noreply.github.com>
2022-11-04 21:01:11 -0500

    Update sumanmum.cpp

--- sumanmum.cpp
+++ sumanmum.cpp
@@ -1,21 +1,22 @@
+// creador: Francisco Garcia
 #include<iostream>
 using namespace std;
 
 int main()
 {
-  float y,s=0.0;
-  int i=0,c;
-  cout<<"Ingrese el valor de c: ";
-  cin>>c;
+  float FMGA_y,s=0.0;
+  int i=0,FMGA_c;
+  cout<<"Ingrese el valor de FMGA_c: ";
+  cin>>FMGA_c;
   do{
 
-  cout<<"Ingrese el valor de y: ";
-  cin>>y;
+  cout<<"Ingrese el valor de FMGA_y: ";
+  cin>>FMGA_y;
   i=i+1;
-  s=s+y;
+  s=s+FMGA_y;
 
 
- }while(i<c);
+ }while(i<FMGA_c);
 
  cout<<"La suma de los numeros fue: "<<s<<endl;
   return 0;
