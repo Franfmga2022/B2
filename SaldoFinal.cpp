5bcce118d15fed28823ca92f8e3af5b823a7a31e
Franfmga2022 <113040688+Franfmga2022@users.noreply.github.com>
2022-11-04 20:54:47 -0500

    Update SaldoFinal.cpp

--- SaldoFinal.cpp
+++ SaldoFinal.cpp
@@ -1,24 +1,25 @@
+// creador: Francisco Garcia
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	float y,s=0;
-	int i=0,c;
-	cout<<"Ingrese cantidad de egresos (c)  : ";
-	cin>>c;
+	float FMGA_y,s=0;
+	int i=0,FMGA_c;
+	cout<<"Ingrese cantidad de egresos (FMGA_c)  : ";
+	cin>>FMGA_c;
 
 	cout<<"Ingrese saldo inicial (s)  : ";
 	cin>>s;
 
 	do{
 	
-	cout<<"Ingrese egreso(y)  : ";
-	cin>>y;
+	cout<<"Ingrese egreso(FMGA_y)  : ";
+	cin>>FMGA_y;
 	i=i+1;
-	s=s+y;
+	s=s+FMGA_y;
 
-        }while(i<c);
+        }while(i<FMGA_c);
 
 	cout<<"El saldo final es :"<<s<<endl;
 	return 0;
