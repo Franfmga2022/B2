f49d8c1cb860c6c7a0c4fcf07801e5f12137abef
Franfmga2022 <113040688+Franfmga2022@users.noreply.github.com>
2022-11-04 20:40:30 -0500

    Update MayorPromedio.cpp

--- MayorPromedio.cpp
+++ MayorPromedio.cpp
@@ -1,20 +1,21 @@
+// creador: Francisco Garcia
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	float y,pm=0;
-	int i=0,c;
-	cout<<"Ingrese c: ";
-	cin>>c;
+	float FMGA_y,pm=0;
+	int i=0,FMGA_c;
+	cout<<"Ingrese FMGA_c: ";
+	cin>>FMGA_c;
 	do{
-		cout<<"Ingrese y: ";
-		cin>>y;
+		cout<<"Ingrese FMGA_y: ";
+		cin>>FMGA_y;
 		i=i+1;
-		if(y>pm){
-			pm=y;
+		if(FMGA_y>pm){
+			pm=FMGA_y;
 	  }
-	}while(i<c);
+	}while(i<FMGA_c);
 	   cout<<"El promedio maximo del curso fue :"<<pm<<endl<<endl;
 	   return 0;
 
