3c8d772fdde74d5d5d5261dfa723f4134031d307
Franfmga2022 <113040688+Franfmga2022@users.noreply.github.com>
2022-11-04 20:35:05 -0500

    Update CuentaMoneda.cpp

--- CuentaMoneda.cpp
+++ CuentaMoneda.cpp
@@ -1,27 +1,28 @@
+// creador: Francisco Garcia
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	float y,s=0,s1=0,s5=0;
-	int i=0,c,i1=0,i5=0;
-	cout<<"Ingrese c: ";
-	cin>>c;
+	float FMGA_y,s=0,s1=0,s5=0;
+	int i=0,FMGA_c,i1=0,i5=0;
+	cout<<"Ingrese FMGA_c: ";
+	cin>>FMGA_c;
 	do{
-		cout<<"Ingrese y: ";
-		cin>>y;
+		cout<<"Ingrese FMGA_y: ";
+		cin>>FMGA_y;
 		i=i+1;
-		s=s+y;
-		if(y==1){
+		s=s+FMGA_y;
+		if(FMGA_y==1){
 		   i1=i1+1;
-		   s1=s1+y;
+		   s1=s1+FMGA_y;
 	        }else{
 
 		 i5=i5+1;
-		 s5=s5+y;
+		 s5=s5+FMGA_y;
 		}
 
-	   }while(i<c);
+	   }while(i<FMGA_c);
 	   cout<<"La cantidad de monedas fue :"<<i<<endl;
 	   cout<<"El valor fue :"<<s<<endl;
 
